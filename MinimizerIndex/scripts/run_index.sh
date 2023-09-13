e35b343257d6b82d6d36804a1d936a9688316d2a
sirishat <sirisha.thippabhotla@ku.edu>
2023-09-13 11:14:47 -0500

    Update run_index.sh

--- MinimizerIndex/scripts/run_index.sh
+++ MinimizerIndex/scripts/run_index.sh
@@ -3,8 +3,8 @@ PROT_DATA=../../data/strep_2x.faa
 EXEC=../bin/mm_index
 
 ## Running the program --DNA overlaps
-/usr/bin/time -f "time result\ncmd:%C\nreal %es\nuser %Us \nsys  %Ss \nmemory %MKB \ncpu %P" $EXEC --seq_type nucl --k 15 --w 3 --target_file $DNA_DATA --query_file $DNA_DATA
+/usr/bin/time -f "time result\ncmd:%C\nreal %es\nuser %Us \nsys  %Ss \nmemory %MKB \ncpu %P" $EXEC --seq_type nucl --k 15 --w 3 --outfile dna.overlaps --target_file $DNA_DATA --query_file $DNA_DATA
 
 
 ## Protein overlaps
-/usr/bin/time -f "time result\ncmd:%C\nreal %es\nuser %Us \nsys  %Ss \nmemory %MKB \ncpu %P" $EXEC --seq_type prot --k 10 --w 3 --target_file $PROT_DATA --query_file $PROT_DATA
+/usr/bin/time -f "time result\ncmd:%C\nreal %es\nuser %Us \nsys  %Ss \nmemory %MKB \ncpu %P" $EXEC --seq_type prot --k 10 --w 3 --outfile protein.overlaps --target_file $PROT_DATA --query_file $PROT_DATA
