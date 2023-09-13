be1a0c0b70fab4339a4f4013cdcb2d2e6897b20d
sirishat <sirisha.thippabhotla@ku.edu>
2023-09-13 11:12:52 -0500

    Update run_index.sh

--- MinimizerIndex/scripts/run_index.sh
+++ MinimizerIndex/scripts/run_index.sh
@@ -1,5 +1,10 @@
-DATA=../../data/strep_2x.fasta
+DNA_DATA=../../data/strep_2x.fasta
+PROT_DATA=../../data/strep_2x.faa
 EXEC=../bin/mm_index
 
-## Running the program
-/usr/bin/time -f "time result\ncmd:%C\nreal %es\nuser %Us \nsys  %Ss \nmemory %MKB \ncpu %P" $EXEC --seq_type nucl --k 10 --w 3 --target_file $DATA --query_file $DATA
+## Running the program --DNA overlaps
+/usr/bin/time -f "time result\ncmd:%C\nreal %es\nuser %Us \nsys  %Ss \nmemory %MKB \ncpu %P" $EXEC --seq_type nucl --k 15 --w 3 --target_file $DNA_DATA --query_file $DNA_DATA
+
+
+## Protein overlaps
+/usr/bin/time -f "time result\ncmd:%C\nreal %es\nuser %Us \nsys  %Ss \nmemory %MKB \ncpu %P" $EXEC --seq_type prot --k 10 --w 3 --target_file $PROT_DATA --query_file $PROT_DATA
